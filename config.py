f69e1329124b35e72dfc2521197c4df37bea2ecd
メ ᴋᴜɴᴀʟ 么 <148003454+APL9210@users.noreply.github.com>
2023-12-30 13:26:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # VALUES REQUIRED FOR XBOTS
 API_ID = 18136872
 API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
-CMD_HNDLR = getenv("CMD_HNDLR", default=".")
+CMD_HNDLR = getenv("CMD_HNDLR", default="!")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
@@ -36,13 +36,13 @@ SUDO_USERS.append(OWNER_ID)
 
 # ------------- CLIENTS -------------
 
-X1 = TelegramClient('X1', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-X2 = TelegramClient('X2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
-X3 = TelegramClient('X3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
-X4 = TelegramClient('X4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
-X5 = TelegramClient('X5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
-X6 = TelegramClient('X6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
-X7 = TelegramClient('X7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
-X8 = TelegramClient('X8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
-X9 = TelegramClient('X9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
-X10 = TelegramClient('X10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
+X1 = TelegramClient('ƉɆ⩔ƗⱠ1', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+X2 = TelegramClient('ƉɆ⩔ƗⱠ2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
+X3 = TelegramClient('ƉɆ⩔ƗⱠ3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
+X4 = TelegramClient('ƉɆ⩔ƗⱠ4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
+X5 = TelegramClient('ƉɆ⩔ƗⱠ5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
+X6 = TelegramClient('ƉɆ⩔ƗⱠ6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
+X7 = TelegramClient('ƉɆ⩔ƗⱠ7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
+X8 = TelegramClient('ƉɆ⩔ƗⱠ8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
+X9 = TelegramClient('ƉɆ⩔ƗⱠ9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
+X10 = TelegramClient('ƉɆ⩔ƗⱠ10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
