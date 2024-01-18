0207bd45e50ffea71069f0c5d19be37c877240da
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-01-18 10:45:42 +0530

    Update bot.py

--- AltBots/modules/bot.py
+++ AltBots/modules/bot.py
@@ -24,7 +24,7 @@ async def ping(e):
         altron = await e.reply(f"✨")
         end = datetime.now()
         mp = (end - start).microseconds / 1000
-        await altron.edit(f"⏤͟͞ ƉɆ⩔ƗⱠ 🍷\n» `{mp} ᴍꜱ`")
+        await altron.edit(f"⏤͟͞ 𝘿𝙀𝙑𝙄𝙇 🍷\n𝙋𝙊𝙉𝙂:🏓» `{mp} ᴍꜱ`")
 
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
