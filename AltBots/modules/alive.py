7800b874bced617bccb4faccdcddf61de1ae82fc
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-02-02 18:30:20 +0530

    Update alive.py

--- AltBots/modules/alive.py
+++ AltBots/modules/alive.py
@@ -16,14 +16,14 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 @X10.on(events.NewMessage(pattern="!alive")) 
 async def start(event):
         AltBot = await event.client.get_me()
-        TEXT = f"╔════❰ **ᴅᴇᴠɪʟ x ɪꜱ ᴀʟɪᴠᴇ 🍁** ❱═❍\n"
+        TEXT = f"╔══❰**ᴅᴇᴠɪʟ x ɪꜱ ᴀʟɪᴠᴇ 🍁**❱═❍\n"
         TEXT += f"║╭━━━━━━━━━━━━━━━➣\n"
         TEXT += f"║┣ » **ᴘʏᴛʜᴏɴ 🐍:** `3.11.3`\n"
         TEXT += f"║┣ » **ᴠᴇʀꜱɪᴏɴ ⚙️:** `M3.4`\n"
-        TEXT += f"║┣ » **ɢʀᴏᴜᴘ 💫:[˹⏤͟͞〲Dᴇᴠɪʟ˹ARC˼🍷˼](https://t.me/+dKGCo7oumwYwZDNl)**\n"
+        TEXT += f"║┣ » **ɢʀᴏᴜᴘ 💫:[˹⏤͟͞〲Dᴇᴠɪʟ](https://t.me/+dKGCo7oumwYwZDNl)**\n"
         TEXT += f"║┣ » **ᴅᴇᴠ 🫂:[˹⏤͟͞〲Dᴇᴠɪʟ 🥀˼](https://t.me/KANU_XD)**\n"
         TEXT += f"║╰━━━━━━━━━━━━━━━➣\n"
-        TEXT += f"╚══════════════════❍"
+        TEXT += f"╚════════════════❍"
         await event.client.send_file(
                     event.chat_id,
                     "https://telegra.ph/file/2f2b8d40e3f2fa4acdc8f.mp4",
