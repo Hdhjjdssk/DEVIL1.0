cdbe4ce50bb55dffdac9c0125251e2b36fbc1e20
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-02-02 12:34:29 +0530

    Update alive.py

--- AltBots/modules/alive.py
+++ AltBots/modules/alive.py
@@ -13,18 +13,19 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 @X7.on(events.NewMessage(pattern="!alive"))
 @X8.on(events.NewMessage(pattern="!alive"))
 @X9.on(events.NewMessage(pattern="!alive"))
-@X10.on(events.NewMessage(pattern="!alive"))
+@X10.on(events.NewMessage(pattern="!alive")) 
 async def start(event):
         AltBot = await event.client.get_me()
-        TEXT = f"**ᴅᴇᴠɪʟ x ɪꜱ ᴀʟɪᴠᴇ 🍁**\n"
-        TEXT += f"┏━━━━━━━━━━━━━━━━\n"
-        TEXT += f"┣•» **ᴘʏᴛʜᴏɴ 🐍:** `3.11.3`\n"
-        TEXT += f"┣•» **ᴠᴇʀꜱɪᴏɴ ⚙️:** `M3.4`\n"
-        TEXT += f"┣•» **ɢʀᴏᴜᴘ 💫:[『 ⏤͟͞〲Dᴇᴠɪʟ˹ARC˼🍷』](https://t.me/+dKGCo7oumwYwZDNl)**\n"
-        TEXT += f"┣•» **ᴅᴇᴠ 🫂:[『⏤͟͞〲Dᴇᴠɪʟ 🥀』](https://t.me/KANU_XD)**\n"
-        TEXT += f"┗━━━━━━━━━━━━━━━━"
+        TEXT = f"╔════❰ **ᴅᴇᴠɪʟ x ɪꜱ ᴀʟɪᴠᴇ 🍁** ❱═❍\n"
+        TEXT += f"║╭━━━━━━━━━━━━━━━➣\n"
+        TEXT += f"║┣ » **ᴘʏᴛʜᴏɴ 🐍:** `3.11.3`\n"
+        TEXT += f"║┣ » **ᴠᴇʀꜱɪᴏɴ ⚙️:** `M3.4`\n"
+        TEXT += f"║┣ » **ɢʀᴏᴜᴘ 💫:[˹⏤͟͞〲Dᴇᴠɪʟ˹ARC˼🍷˼](https://t.me/+dKGCo7oumwYwZDNl)**\n"
+        TEXT += f"║┣ » **ᴅᴇᴠ 🫂:[˹⏤͟͞〲Dᴇᴠɪʟ 🥀˼](https://t.me/KANU_XD)**\n"
+        TEXT += f"║╰━━━━━━━━━━━━━━━➣\n"
+        TEXT += f"╚══════════════════❍"
         await event.client.send_file(
                     event.chat_id,
-                    "https://graph.org/file/b0c9f1dca2f5d3ce84835.jpg",
+                    "https://telegra.ph/file/2f2b8d40e3f2fa4acdc8f.mp4",
                     caption=TEXT, 
                 )
